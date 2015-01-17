499c65ba8139b60ca0fadbbd50f28afb846ddb2b
Shaunak Pagnis <shaunak@amuratech.com>
2015-01-17 13:21:12 +0530

    jquery, account-ui and account-passwords packages added

--- online-election/models.js
+++ online-election/models.js
@@ -1,4 +1,12 @@
 /**
 * Models
 */
-Users = new Meteor.Collection('users');
+//Users = new Meteor.Collection('users');
+
+Elections = new Meteor.Collection('elections'); 
+
+//fields: region string 
+//start_date: DateTime
+//end_date: DateTime
+//candidate_requirements: Hash
+//*voter_requirements: Hash
